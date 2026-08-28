19cd2e18dbdae4aff592d058264f47f27112e793
Sameer <sameer21102000@gmail.com>
2026-08-28 14:56:22 +0530

    :

--- app/src/main/java/com/sap/training/HelloController.java
+++ app/src/main/java/com/sap/training/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello Sameer from SAP DevOps Training!";
+        return "Hello Sameer froasdfasdfm SAP DevOps Training!";
     }
 
 //    @GetMapping("/{id}")
